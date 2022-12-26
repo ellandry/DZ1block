8f095b55696037da4c5bef4393c36e2c41836b62
ellandry <ellandry777@mail.ru>
2022-12-26 13:24:35 +0300

    Create metod FillArry

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача. Написать программу, которая из имеющего массива строк формирует массив 
+// строк, длина которых меньше или равна 3 символа. Первоначальный массив можно ввести 
+// с клавиатуры, либо задать на старте выполнения алгоритма. При решении не рекомендуется 
+// пользоваться коллекциями, лучше обойтись исключительно массивами.
+// Примеры:
+// ["hello", "2", "world", ":-)"] -> ["2", ":-)"]
+// ["1234", "1567", "-2", "computer science"] -> ["-2"]
+// ["Russia", "Denmark", "Kazan"] -> []
+using System.Linq;
+string[] FillArray(string text)
+{
+    if (text == "") return new string[] {""};
+    else
+    {    
+    string[] textArray = text.Split(" ")
+                            .ToArray();
+    return textArray;
+    }
+}
+
